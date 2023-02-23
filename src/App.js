a40a6e738a5f04e113122369a39acb809241d28a
Diwakar Parmar <diwakarparmar@gmail.com>
2023-02-22 20:21:55 -0500

    Firebase + useEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartVisible = useSelector(store => store.ui.cartVisible);
+
+  const cart = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-f9fe3-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
