63f8b480c286bff815ef8e4dd94914a84d078745
Diwakar Parmar <diwakarparmar@gmail.com>
2023-02-23 00:00:18 -0500

    added thunk for updating firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { showNotification } from "./store/notificationSlice";
+import { sendCartData } from "./store/cartSlice";
 
 let isInitial = true;
 
@@ -24,45 +24,7 @@ function App() {
       return;
     }
 
-    dispatch(
-      showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const sendCartData = async () => {
-      const response = await fetch(
-        "https://react-http-f9fe3-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    sendCartData().catch((error) => {
-      dispatch(
-        showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { showNotification } from "./notificationSlice";
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -7,6 +9,10 @@ const cartSlice = createSlice({
     totalQuatity: 0,
   },
   reducers: {
+    replaceCart: (state, action) => {
+      state.totalQuatity = action.payload.totalQuatity;
+      state.items = action.payload.item;
+    },
     addItem: (state, action) => {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -42,5 +48,52 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addItem, removeItem } = cartSlice.actions;
+// Action Creator
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-f9fe3-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const { replaceCart, addItem, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
